8f90de16fdc11ef2ce8682f63c913d79f7f2db0e
Anonymous Meerkat <meerkatanonymous@gmail.com>
2014-07-01 15:47:46 -0600

    Added vignette and greyscale
    Also cleaned up the code a little

--- glsl/zblur/fragment.glsl
+++ glsl/zblur/fragment.glsl
@@ -8,8 +8,11 @@ uniform float N_alpha;
 uniform int N_time;
 uniform int N_rand;
 
-const float sampleDist = 0.3;
-const float sampleStrength = 7.2;
+const float sample_dist = 0.3;
+const float sample_strength = 7.2;
+
+const float samples[12] =
+   float[](-0.1, -0.08, -0.05, -0.03, -0.02, -0.01, 0.01, 0.02, 0.03, 0.05, 0.08, 0.1);
 
 float rand(vec2 n)
 {
@@ -17,51 +20,38 @@ float rand(vec2 n)
      fract(sin(dot(vec2(mod(N_rand, n.x), mod(N_rand, n.y)), vec2(12.9898, 78.233)))* (43758.5453));
 }
 
-void main() {
-    // some sample positions
-   float samples[12] =
-   float[](-0.1,-0.08,-0.05,-0.03,-0.02,-0.01,0.01,0.02,0.03,0.05,0.08,0.1);
+void rad_blur() {
+    vec2 player_dir = pos - UV;
+    float player_dist = length(player_dir);
+    player_dir /= player_dist;
 
-    // 0.5,0.5 is the center of the screen
-    // so substracting uv from it will result in
-    // a vector pointing to the middle of the screen
-    vec2 dir = pos - UV;
+    vec4 sum = out_color;
 
-    // calculate the distance to the center of the screen
-    float dist = sqrt(dir.x*dir.x + dir.y*dir.y);
-
-    // normalize the direction (reuse the distance)
-    //dir = dir/dist;
-    dir /= dist;
-
-    // this is the original colour of this fragment
-    // using only this would result in a nonblurred version
-    vec4 color = texture2D(texsampler,UV);
-
-    vec4 sum = color;
-
-    // take 10 additional blur samples in the direction towards
-    // the center of the screen
-    for (int i = 0; i < 12; i++)
-    {
-      sum += texture2D( texsampler, UV + dir * samples[i] * sampleDist );
+    for (int i = 0; i < 12; i++) {
+      sum += texture2D(texsampler, UV + player_dir * samples[i] * sample_dist);
     }
 
-    // we have taken eleven samples
-    sum *= 1.0/13.0;
+    sum *= 1./13.;
 
-    // weighten the blur effect with the distance to the
-    // center of the screen ( further out is blurred more)
-    float t = dist * sampleStrength;
-    t = clamp( t ,0.0,1.0); //0 <= t <= 1
+    float t = player_dist * sample_strength;
+    t = clamp(t, 0., 1.);
 
-    //Blend the original color with the averaged pixels
-    out_color = mix( color, sum, t );
-    //out_color = texture2D(texsampler, UV);
-    t = 1 - dist*2;
-    t = smoothstep(.7,0,dist);
-    out_color *= vec4(t, t, t, N_alpha);
+    out_color = mix(out_color, sum, t);
+}
 
+void vignette() {
+    vec2 center_dir = 0.5 - UV;
+    float center_dist = length(center_dir);
+    float t = smoothstep(.75, .3, center_dist);
+    out_color.rgb = mix(out_color.rgb, out_color.rgb * t, 0.5);
+}
+
+void greyscale() {
+    float t = dot(out_color.rgb, vec3(0.299, 0.587, 0.114));
+    out_color.rgb = mix(out_color.rgb, vec3(t), 0.5);
+}
+
+void film_noise() {
     float x = rand(UV);
     vec4 grain = vec4(mod((mod(x, 13) + 1) * (mod(x, 123) + 1), 0.01)-0.005) * 16;
     //vec4 grain = vec4(mod((mod(x, 13) + 1) * (mod(x, 123) + 1), 0.01));
@@ -69,4 +59,14 @@ void main() {
     out_color += grain;
 
     out_color -= rand(vec2(N_rand, N_time)) * 0.1;
+}
+
+void main() {
+    out_color = texture2D(texsampler, UV);
+    out_color.a *= N_alpha;
+
+    rad_blur();
+    vignette();
+    greyscale();
+    film_noise();
 }
